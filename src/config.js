2554aec23fef59a4f14fce271f0e7b01d80ae8c9
jack eichner <jack8191@gmail.com>
2019-03-22 00:09:23 -0700

    trying to get exports to work

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 module.exports = {
     PORT: process.env.PORT || 3000,
     // other stuff
-    API_BASE_URL: process.env.REACT_APP_API_BASE_URL ||
-      "http://localhost:8000",
+    API_BASE_URL: process.env.REACT_APP_API_BASE_URL 
+      ,
       devServer: {
         historyApiFallback: true
       },
